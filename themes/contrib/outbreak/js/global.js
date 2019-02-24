7241f0a57acdac7c2901a9ea51bd10d5decdb8de
LukeSNolan <44662488+LukeSNolan@users.noreply.github.com>
2019-02-24 21:54:16 +1100

    Carousel js and css changes. Added FontAwesome 5 Free font.

--- themes/contrib/outbreak/js/global.js
+++ themes/contrib/outbreak/js/global.js
@@ -37,39 +37,59 @@ jQuery(".region.region-primary-menu span.sub-menu-arrow").keypress(function(e){
 });
 
 var myResponse;
-var carouselHtml = "<div class='carousel' data-slick='{\"slidesToShow\": 1, \"slidesToScroll\": 1, \"swipe\": false, \"dots\": true, \"arrows\": true}'>";
+
 //Home Carousel
-(function insertOutbreakCarousel() {   
+(function insertOutbreakCarousel() {
     var queryUrl = "rest/views/outbreak-responses-carousel";
+    var carouselHtml = "<div class='carousel' data-slick='{\"arrows\": false, \"dots\": true, \"swipe\": false, \"fade\": true}'>";
+    var carouselStatePickerHtml = "<ul class='carousel-state-selector-list'>";
 
     executeQuery(queryUrl, function(r){
         myResponse = r;
         var lastState = "";
+        var currentState;
+        var stateIndex = 0;
+        var noCurrentResponsesText = "No current responses";
 
         jQuery.each(r, function() {
+            currentState = this.state;
+            
+            if(!this.acronym == "") {
+                noCurrentResponsesText += " (" + this.acronym + ")";
+            }
 
-            if (this.state != lastState) {
-
+            if (currentState != lastState && currentState != "New South Wales") {
+                
                 if (lastState != "") {
-                    carouselHtml += "</ul></div></div></div>";
+                    carouselHtml += "</ul></div></div>";
+                    stateIndex++;
+                }
+                
+                lastState = currentState;
+
+                if(currentState == "Australian Capital Territory") {
+                    currentState = "New South Wales & Australian Capital Territory";
                 }
                 
                 lastState = this.state;
                 carouselHtml += "<div class='carousel-item'>";
-                carouselHtml += "<div class='carousel-item-heading'>Current responses to outbreaks: " + this.state + "</div>";
+                carouselHtml += "<h2 class='carousel-item-heading'>Current responses to outbreaks: " + currentState + "</h2>";
                 carouselHtml += "<div class='carousel-item-main-container'>";
-                carouselHtml += "<div class='carousel-item-image'><img src='" + this.state_image + "' alt='Map of " + this.state + " region'></div>";
-                carouselHtml += "<div class='carousel-item-list'><ul><li class='carousel-item-li'>";
-                carouselHtml += (this.outbreak != "") ? "<a href='" + this.outbreak_link + "'>" + this.outbreak + "</a>" : "No current responses";
+                carouselHtml += "<img class='carousel-item-image' src='" + this.state_image + "' alt='Map of " + currentState + " region'>";
+                carouselHtml += "<ul class='carousel-item-list'>";
+                carouselHtml += (this.outbreak != "") ? "<li class='carousel-item-li'><a href='" + this.outbreak_link + "'>" + this.outbreak + "</a>" : "<li class='carousel-item-li no-response'>" + noCurrentResponsesText;
                 carouselHtml += "</li>";
+                carouselStatePickerHtml += "<li class='carousel-state-selector-list-item' aria-hidden='true'><button type='button' data-slide='" + stateIndex + "'>" + currentState + "</button></li>"
             } else {
-                carouselHtml += "<a href='" + this.outbreak_link + "'>" + this.outbreak + "</a>";
-            }  
+                carouselHtml += (this.outbreak != "") ? "<li class='carousel-item-li'><a href='" + this.outbreak_link + "'>" + this.outbreak + "</a>" : "<li class='carousel-item-li no-response'>" + noCurrentResponsesText;
+                carouselHtml += "</li>";
+            }
 
+            noCurrentResponsesText = "No current responses";
         });
 
-        carouselHtml += "</ul></div></div></div></div>";
-        jQuery('#block-homepagecarousel').html(carouselHtml);
+        carouselHtml += "</ul></div></div></div>";
+        jQuery('#block-homepagecarousel').html(carouselHtml + carouselStatePickerHtml);
         jQuery('.carousel').slick();
     });
     
